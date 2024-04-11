db413ef51cf9ea97b64b4cce2ae83ec43b5e55e2
StarKnight <prasenjitt4e@gmail.com>
2024-04-11 22:42:22 +0530

    added icons in the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <>
+      <Sidebar />
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
-function Sidebar() {
+const Sidebar = () => {
   return (
     <div className='sidebar'>
-      
+     <div className="top">
+      <img className='menu' src={assets.menu_icon} alt="" />
+      <div className="new-chat">
+        <img src={assets.plus_icon} alt="" />
+        <p>New Chat</p>
+      </div>
+      <div className="recent">
+        <p className="recent-title">Recent</p>
+        <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is React ...</p>
+        </div>
+      </div>
+     </div>
+    <div className="bottom">
+      <div className="bottom-item recent-entry">
+        <img src={assets.question_icon} alt="" />
+        <p>Help</p>
+      </div>
+      <div className="bottom-item recent-entry">
+        <img src={assets.history_icon} alt="" />
+        <p>Activity</p>
+      </div>
+      <div className="bottom-item recent-entry">
+        <img src={assets.setting_icon} alt="" />
+        <p>Settings</p>
+      </div>
+    </div>
     </div>
   )
 }
