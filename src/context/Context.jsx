27832d8f91335342dc61ef4dc0d5075ca119a8f9
StarKnight <prasenjitt4e@gmail.com>
2024-04-12 17:56:00 +0530

    added typing effect

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -12,6 +12,14 @@ const ContextProvider = (props) =>{
     const [loading,setLoading] = useState(false)
     const [resultData,setResultData] = useState("");
 
+    // Typing effect logic
+
+    const delayPara = (index,nextWord) =>{
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+
     const onSent = async (prompt)=>{
 
       setResultData("")
@@ -19,7 +27,26 @@ const ContextProvider = (props) =>{
       setShowResult(true)
       setRecentPrompt(input)
     const response = await runChat(input)
-    setResultData(response)
+    let responseArray = response.split("**");
+    let newResponse;
+    for(let i = 0; i < responseArray.length; i++)
+    {
+      if (i === 0 || i%2 !== 1){
+         newResponse += responseArray[i];
+      }
+      else{
+        newResponse += "<b>"+responseArray[i]+ "</b>"
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>")
+    let newResponseArray = newResponse2.split(" ");
+
+    for(let i =0; i<newResponseArray.length;i++)
+    {
+    const nextWord = newResponseArray[i];
+    delayPara(i,nextWord+" ")
+    }
+
     setLoading(false)
     setInput("")
 
