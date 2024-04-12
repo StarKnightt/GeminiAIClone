47fc14f63a633a0ec71d00eb69b5a9b8e82d1455
StarKnight <prasenjitt4e@gmail.com>
2024-04-12 20:44:09 +0530

    fixed the newchat button

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
     <div className="sidebar">
       <div className="top">
         <img onClick={()=>setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
-        <div onClick={()=>newChat} className="new-chat">
+        <div onClick={()=>newChat()} className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
