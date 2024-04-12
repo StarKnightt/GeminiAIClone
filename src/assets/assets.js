4f10372d994953735392f2d49fcca344eb9b2cef
StarKnight <prasenjitt4e@gmail.com>
2024-04-12 22:06:52 +0530

    Added GitHub icon and updated assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -13,6 +13,7 @@ import message_icon from './message_icon.png'
 import code_icon from './code_icon.png'
 import send_icon from './send_icon.png'
 import gemini_icon from './gemini_icon.png'
+import github_icon from './github_icon.png'
 
 export const assets = {
     history_icon,
@@ -29,5 +30,6 @@ export const assets = {
     message_icon,
     code_icon,
     send_icon,
-    gemini_icon
+    gemini_icon,
+    github_icon
 }
