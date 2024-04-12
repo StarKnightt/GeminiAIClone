a7a13021f11b9cd47db2ef65854374d820a0f510
StarKnight <prasenjitt4e@gmail.com>
2024-04-12 15:33:23 +0530

    search box almost completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -34,6 +34,19 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                Gemini may display inaccurate info, including about people, so double-check its responses. Your Privacy and Gemini Apps
+                </p>
+            </div>
         </div>
     </div>
   )
