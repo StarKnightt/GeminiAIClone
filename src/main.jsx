06bab234d8624bc104baf0e4023901d381c4166f
StarKnightt <prasenjitt4e@gmail.com>
2024-05-28 15:09:34 +0530

    added some comments so that it would be easy to understand

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import ContextProvider from "./context/Context.jsx";
+import React from "react"; // Importing React library
+import ReactDOM from "react-dom/client"; // Importing ReactDOM for rendering
+import App from "./App.jsx"; // Importing the main App component
+import "./index.css"; // Importing CSS file for styling
+import ContextProvider from "./context/Context.jsx"; // Importing the ContextProvider component
 
+// Rendering the root component of the application
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ContextProvider>
-    <App />
+  <ContextProvider> {/* Wrapping the App component with the ContextProvider */}
+    <App /> {/* Rendering the main App component */}
   </ContextProvider>
 );
