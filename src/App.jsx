79e0bb8f23fd2c6fadc5975c784cd96afb13dfa0
StarKnightt <prasenjitt4e@gmail.com>
2024-05-28 15:10:10 +0530

    added some comments so that it would be easy to understand

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Sidebar from "./components/Sidebar/Sidebar";
-import Main from "./components/Main/Main";
+import Sidebar from "./components/Sidebar/Sidebar"; // Importing the Sidebar component
+import Main from "./components/Main/Main"; // Importing the Main component
+
 function App() {
-	return (
-		<>
-			<Sidebar />
-			<Main />
-		</>
-	);
+  return (
+    <>
+      <Sidebar /> {/* Rendering the Sidebar component */}
+      <Main /> {/* Rendering the Main component */}
+    </>
+  );
 }
 
-export default App;
+export default App; // Exporting the App component as default
